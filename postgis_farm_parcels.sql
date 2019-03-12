10c8e1fa89dd2f41f44334039131b276c7542a76
Jason <jasonmcvay09@gmail.com>
2019-03-12 09:50:20 -0700

    updated "geom" field name

--- postgis_farm_parcels.sql
+++ postgis_farm_parcels.sql
@@ -1,5 +1,5 @@
 CREATE TABLE farm_parcels AS
-SELECT p.owner, p.taxbill_ad, p.taxbill_cs, ST_Multi(ST_UNION(p.geom))
+SELECT p.owner, p.taxbill_ad, p.taxbill_cs, ST_Multi(ST_UNION(p.geom)) as geom
 FROM parcels_ilmenard p
 WHERE farm_acres > 0
 GROUP BY p.owner, p.taxbill_ad, p.taxbill_cs;
