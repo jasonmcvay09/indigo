20c83dd855279d33cbbb6d8f094c4edbfdb5e68c
Jason <jasonmcvay09@gmail.com>
2019-03-12 10:32:50 -0700

    cast avg_di as numeric

--- silo_count.sql
+++ silo_count.sql
@@ -1,6 +1,6 @@
-CREATE TABLE farm_silo_count
+CREATE TABLE farm_silo_count2
 AS
-SELECT f.owner, f.taxbill_ad, f.taxbill_cs, f.geom, count(silos_ilmenard.geom) AS silo_count, avg(silos_ilmenard.diameter) AS silo_avg_di 
+SELECT f.owner, f.taxbill_ad, f.taxbill_cs, f.geom, COUNT(silos_ilmenard.geom) AS silo_count, ROUND(AVG(silos_ilmenard.diameter)::numeric,1) AS silo_avg_di 
 FROM farm_parcels2 f JOIN silos_ilmenard 
 ON ST_Contains(f.geom, silos_ilmenard.geom) 
 GROUP BY f.owner, f.taxbill_ad, f.taxbill_cs, f.geom
