809738675d7ab470c1d7b7f58d34d5ca894f0109
Jason <jasonmcvay09@gmail.com>
2019-03-12 10:19:45 -0700

    included avg silo diameter, count

--- silo_count.sql
+++ silo_count.sql
@@ -1,5 +1,5 @@
-SELECT farm_parcels2.owner, count(silos_ilmenard.geom) AS silo_count 
+SELECT farm_parcels2.owner, farm_parcels2.taxbill_ad, farm_parcels2.taxbill_cs, farm_parcels2.geom, count(silos_ilmenard.geom) AS silo_count, avg(silos_ilmenard.diameter) AS silo_avg_di 
 FROM farm_parcels2 JOIN silos_ilmenard 
 ON ST_Contains(farm_parcels2.geom, silos_ilmenard.geom) 
-GROUP BY farm_parcels2.owner
+GROUP BY farm_parcels2.owner, farm_parcels2.taxbill_ad, farm_parcels2.taxbill_cs, farm_parcels2.geom
 ORDER BY silo_count DESC;
