11f4e2fcfe15a44aafeb7a6a5914e6a2bdb4b59a
Jason <jasonmcvay09@gmail.com>
2019-03-12 21:39:06 -0700

    fixed typo

--- silo_count_min_max_med.sql
+++ silo_count_min_max_med.sql
@@ -5,7 +5,7 @@ COUNT(s.geom) AS silo_count,
 ROUND(AVG(s.diameter)::numeric,1) AS silo_avg_di,
 SUM(s.low_bushel) AS low_bushel_sum,
 SUM(s.high_bushe) AS high_bushel_sum,
-SUM(s.median_bus) as med_bushel_sum
+SUM(s.median_bus) AS med_bushel_sum
 FROM farm_parcels2 f JOIN silo_bushels s 
 ON ST_Contains(f.geom, s.geom) 
 GROUP BY f.owner, f.taxbill_ad, f.taxbill_cs, f.geom
