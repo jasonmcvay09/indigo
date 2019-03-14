cabbc99f0da3e0d4f203b075a20b85e3285f7d55
Jason <jasonmcvay09@gmail.com>
2019-03-13 18:08:58 -0700

    Delete silo_count.sql

--- silo_count.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE farm_silo_count2
-AS
-SELECT f.owner, f.taxbill_ad, f.taxbill_cs, f.geom, COUNT(silos_ilmenard.geom) AS silo_count, ROUND(AVG(silos_ilmenard.diameter)::numeric,1) AS silo_avg_di 
-FROM farm_parcels2 f JOIN silos_ilmenard 
-ON ST_Contains(f.geom, silos_ilmenard.geom) 
-GROUP BY f.owner, f.taxbill_ad, f.taxbill_cs, f.geom
-ORDER BY silo_count DESC;
